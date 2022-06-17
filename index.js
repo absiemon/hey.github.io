b747f0df24cbb1d08d3a06cbddf04b92b659b948
Abhay <abc@gmail.com>
2022-06-17 19:20:18 +0530

    set up ejs view engine

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const port = 8001;
 //using express router with the help of the middleware
 app.use('/', require('./routes'));  // whether the request comes from home page sends to the routs folder
 
+//------- setting up the view engine-----------------
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-
+//----------------------------------------------------
 
 
 
